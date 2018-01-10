cbf845918aa74137ff8897c4b0804a6af70c6035
Ryan Jackman <rtj4953@rit.edu>
2018-01-10 01:03:08 -0500

    represented all data for each element

--- src/Element.jsx
+++ src/Element.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import elementData from './moredata.json';
 import styled from 'styled-components';
+import ordinal from 'ordinal';
 
 const ElementPage = styled.div`
   margin-left: 250px;
@@ -57,12 +58,48 @@ const category_name = [
 
 class Element extends Component {
 
-  formatDiscoveryYear(year) {
+  formatDiscoveryYear(num) {
+    const year = elementData.discover[num];
     if(!year) {
       return null;
     }
     const formattedYear = year > 0 ? year + "AD" : Math.abs(year) + "BC";
     return <p>Discovered: {formattedYear}</p>;
+  }
+
+  formatAbundances(num) {
+    return (
+      <div>
+        <strong>Abundance</strong>
+        <div style={{"margin-left": "2em"}}>
+          <p>Universe: {elementData.abundance.universe[num]}%</p>
+          <p>Solar System: {elementData.abundance.solar[num]}%</p>
+          <p>Meteor: {elementData.abundance.meteor[num]}%</p>
+          <p>Crust: {elementData.abundance.crust[num]}%</p>
+          <p>Ocean: {elementData.abundance.ocean[num]}%</p>
+          <p>Human: {elementData.abundance.human[num]}%</p>
+        </div>
+      </div>
+    );
+  }
+
+  formatIonizationEnergies(num) {
+    let ionizations = [];
+
+    elementData.ionize.forEach((level) => {
+      if (num in level) {
+        ionizations.push(level[num]);
+      } else {
+        return;
+      }
+    });
+
+    let str = "";
+    ionizations.forEach((level, i) => {
+      str += ordinal(parseInt(i + 1, 10)) + ": " + level + " ";
+    });
+
+    return <p>Ionization Energies: {str}</p>;
   }
 
   render() {
@@ -77,17 +114,46 @@ class Element extends Component {
         <ElementDetails>
           <p>Category: {category_name[elementData.category[num]]}</p>
           <p>Electronegativity: {elementData.electroneg[num]}</p>
-          {this.formatDiscoveryYear(elementData.discover[num])}
+          {this.formatDiscoveryYear(num)}
           <p>Electron Affinity: {elementData.affinity[num]} kJ/mol</p>
-          <p>Heat</p>
-          <p style={{"text-indent": "2em"}}>Specific: {elementData.heat.specific[num]} J/kg/K</p>
-          <p style={{"text-indent": "2em"}}>Vaporization: {elementData.heat.vaporization[num]} kJ/mol</p>
-          <p style={{"text-indent": "2em"}}>Fusion: {elementData.heat.fusion[num]} kJ/mol</p>
-          <p>Radius</p>
-          <p style={{"text-indent": "2em"}}>Calculated: {elementData.radius.calculated[num]} pm</p>
-          <p style={{"text-indent": "2em"}}>Empirical: {elementData.radius.empirical[num]} pm</p>
-          <p style={{"text-indent": "2em"}}>Covalent: {elementData.radius.covalent[num]} pm</p>
-          <p style={{"text-indent": "2em"}}>Van der Waals: {elementData.radius.vanderwaals[num]} pm</p>
+          <strong>Heat</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>Specific: {elementData.heat.specific[num]} J/kg/K</p>
+            <p>Vaporization: {elementData.heat.vaporization[num]} kJ/mol</p>
+            <p>Fusion: {elementData.heat.fusion[num]} kJ/mol</p>
+          </div>
+          <strong>Radius</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>Calculated: {elementData.radius.calculated[num]} pm</p>
+            <p>Empirical: {elementData.radius.empirical[num]} pm</p>
+            <p>Covalent: {elementData.radius.covalent[num]} pm</p>
+            <p>Van der Waals: {elementData.radius.vanderwaals[num]} pm</p>
+          </div>
+          {this.formatAbundances(num)}
+          {this.formatIonizationEnergies(num)}
+          <p>Valence: {elementData.valence[num]}</p>
+          <strong>Density</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>STP: {elementData.density.stp[num]} kg/m<sup>3</sup></p>
+            <p>Liquid: {elementData.density.liquid[num]} kg/m<sup>3</sup></p>
+          </div>
+          <strong>Hardness</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>Brinell: {elementData.hardness.brinell[num]} Mpa</p>
+            <p>Mohs: {elementData.hardness.mohs[num]}</p>
+            <p>Vickers: {elementData.hardness.vickers[num]} Mpa</p>
+          </div>
+          <strong>Modulus</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>Bulk: {elementData.modulus.bulk[num]} GPa</p>
+            <p>Shear: {elementData.modulus.shear[num]} GPa</p>
+            <p>Young: {elementData.modulus.young[num]} GPa</p>
+          </div>
+          <strong>Conductivity</strong>
+          <div style={{"margin-left": "2em"}}>
+            <p>Thermal: {elementData.conductivity.thermal[num]} W/mK</p>
+            <p>Electric: {elementData.conductivity.electric[num]} MS/m</p>
+          </div>
         </ElementDetails>
         <div />
       </ElementPage>
